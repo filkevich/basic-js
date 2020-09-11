164d276c004ccdefa7fb6c1eb8e648b6e864f8ef
artiomfilkevich <artiom.filkevich@gmail.com>
2020-09-11 11:52:50 +0300

    feat: implement count-cats, dream-team, what-season

--- src/dream-team.js
+++ src/dream-team.js
@@ -1,6 +1,16 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function createDreamTeam(/* members */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function createDreamTeam(array) {
+  let newArray = [];
+
+  if (!Array.isArray(array)) {
+    return false;
+  }
+  
+  array.forEach(element => {
+    if (typeof element == 'string') {
+      newArray.push(element.trim()[0].toUpperCase());
+    }
+  });
+  return (newArray.sort().join(''));
 };
