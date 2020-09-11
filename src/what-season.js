5645b8f3bf70077ebd8f6cfba2885d379b4f19e0
artiomfilkevich <artiom.filkevich@gmail.com>
2020-09-11 11:52:50 +0300

    feat: implement count-cats, dream-team, what-season

--- src/what-season.js
+++ src/what-season.js
@@ -1,6 +1,23 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function getSeason(/* date */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function getSeason(date) {
+  if (date == undefined) {
+    return 'Unable to determine the time of year!';
+  }
+
+  if (!(Object.prototype.toString.call(date) === '[object Date]')) {
+    throw 'Unable to determine the time of year!';
+  }
+
+  let month = date.getMonth();
+  if (month < 2 || month == 11) {
+    return 'winter';
+  } else if (month > 1 && month < 5) {
+    return 'spring';
+  } else if (month > 4 && month < 8) {
+    return 'summer';
+  } else {
+    return 'autumn';
+  }
+
 };
