d928fe5066472963a98e2294f449c035b46c9080
Artiom <artiom.filkevich@gmail.com>
2020-10-05 17:45:39 +0300

    refactor: remove console.logs in carbon-dating task solution

--- src/carbon-dating.js
+++ src/carbon-dating.js
@@ -5,17 +5,12 @@ const HALF_LIFE_PERIOD= 5730;
 
 module.exports = function dateSample(sampleActivity) {
   if (typeof sampleActivity != 'string' || isNaN(sampleActivity)) { 
-    console.log ('=== FALSE ===');
-    console.log (sampleActivity);
     return false; 
   } 
   else {
     if (sampleActivity < 16 || sampleActivity >= 9000) {
       return false;
     }
-    console.log ('=== TRUE ===');
-    console.log (sampleActivity);
-    console.log (Math.ceil((Math.log(Math.abs(MODERN_ACTIVITY/sampleActivity)))*HALF_LIFE_PERIOD/0.693));
     return Math.ceil((Math.log(MODERN_ACTIVITY/sampleActivity))*HALF_LIFE_PERIOD/0.693);
   }
 };
